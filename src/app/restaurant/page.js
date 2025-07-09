a5a012c028a16b0e079ad100bd02a29fdb71f09c
Gideon Abe <gideonabe2020@gmail.com>
2025-07-09 17:02:47 +0100

    more commit

--- src/app/restaurant/page.js
+++ src/app/restaurant/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import Home from "./pages/Home";
-
+ 
+import Restaurantdetails from "../pages/Restaurantdetails";
 export default function App() {
   return (
     <>
-        <Home />
+        <Restaurantdetails />
 
     </>
   );
